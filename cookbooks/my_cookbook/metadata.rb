642ba5f1a62cb3c2f8504d7b9bb246052e68fc16
Jim McMahon <bcxk8mn@gmail.com>
2013-09-21 15:43:21 -0400

    Add basic cookbook without recipes

---
+++ cookbooks/my_cookbook/metadata.rb
@@ -0,0 +1,7 @@
+name             'my_cookbook'
+maintainer       'fuzzyfir.com'
+maintainer_email 'bcxk8mn@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures my_cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
